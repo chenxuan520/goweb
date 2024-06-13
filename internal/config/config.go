0324c2600d8b4612bebe3d456d75e2e72073c02d
chenxuan <1607772321@qq.com>
2024-06-13 21:33:16 +0800

    feat:init

---
+++ internal/config/config.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+type Server struct {
+	Port     int    `json:"port"`
+	Password string `json:"password"`
+}
+
+type Config struct {
+	Server Server `json:"server"`
+}
+
+var (
+	GlobalConfig *Config
+)
+
+func Init() {
+	configFile := "./config/config.json"
+	data, err := ioutil.ReadFile(configFile)
+
+	if err != nil {
+		log.Println(err)
+		configFile = "config.json"
+		data, err = ioutil.ReadFile("/config/" + configFile)
+		if err != nil {
+			log.Println("Read config error!")
+			log.Panic(err)
+			return
+		}
+	}
+
+	config := &Config{}
+
+	err = json.Unmarshal(data, config)
+
+	if err != nil {
+		log.Println("Unmarshal config error!")
+		log.Panic(err)
+		return
+	}
+
+	GlobalConfig = config
+	log.Println("Config " + configFile + " loaded.")
+}
+
+func InitWithPath(configPath string) error {
+	data, err := ioutil.ReadFile(configPath)
+
+	if err != nil {
+		return err
+	}
+
+	config := &Config{}
+
+	err = json.Unmarshal(data, config)
+
+	if err != nil {
+		log.Println("Unmarshal config error!")
+		return err
+	}
+
+	GlobalConfig = config
+	log.Println("Config " + configPath + " loaded.")
+	return nil
+}
