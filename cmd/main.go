e1b5692e5d9bb043bf0e12a0ad1b755b234ad070
chenxuan <1607772321@qq.com>
2024-09-22 13:59:05 +0800

    feat:update add log

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/chenxuan520/goweb/internal/config"
 	"github.com/gin-gonic/gin"
@@ -13,5 +14,7 @@ func main() {
 	for _, staticPath := range config.GlobalConfig.StaticPaths {
 		g.Static(staticPath.WebPath, staticPath.FilePath)
 	}
+	// print path of server
+	log.Println("Server is running at http://" + config.GlobalConfig.Server.Host + ":" + fmt.Sprintf("%d", config.GlobalConfig.Server.Port))
 	g.Run(config.GlobalConfig.Server.Host + ":" + fmt.Sprintf("%d", config.GlobalConfig.Server.Port))
 }
